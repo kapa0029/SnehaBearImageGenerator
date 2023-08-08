5fad44f28b723d783469aa60050fd8e1c6401cfd
Sneha Ka Patel <kapa0029@algonquinlive.com>
2023-08-07 21:31:19 -0400

    Added toolbar to activity_bear.xml and activity_bear_saved_images.xml

--- app/src/main/java/com/example/final_project_android/BearSavedImagesActivity.java
+++ app/src/main/java/com/example/final_project_android/BearSavedImagesActivity.java
@@ -1,19 +1,26 @@
 package com.example.final_project_android;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.final_project_android.databinding.ActivityBearBinding;
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.List;
 
 public class BearSavedImagesActivity extends AppCompatActivity implements MyBearAdapter.OnItemClickListener, MyBearAdapter.OnItemLongClickListener {
-
+    ActivityBearBinding binding;
     private RecyclerView recyclerView;
     private MyBearAdapter adapter;
     private List<BearItemEntity> bearItems;
@@ -22,10 +29,17 @@ public class BearSavedImagesActivity extends AppCompatActivity implements MyBear
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityBearBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_bear_saved_images);
 
         recyclerView = findViewById(R.id.recyclerViewSavedImages);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        Toolbar toolbar = findViewById(R.id.bearToolbar); // Find the Toolbar by its ID
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Bear Image Generator");
+        getSupportActionBar().setSubtitle("Saved Images");
 
         // Retrieve bearItems from the database using the BearItemRepository
         BearItemRepository bearItemRepository = new BearItemRepository(getApplication());
@@ -77,6 +91,35 @@ public class BearSavedImagesActivity extends AppCompatActivity implements MyBear
                     Toast.makeText(this, "Image deleted", Toast.LENGTH_SHORT).show();
                 })
                 .setNegativeButton("No", null)
+                .show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.helpBear) {
+            showAlertDialog();
+            //Toast.makeText(this, "Click on an image to see its dimensions", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        // Handle other menu items if needed
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_bear, menu);
+        return true;
+    }
+    private void showAlertDialog() {
+        androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(this);
+        builder.setTitle("How to use")
+                .setMessage("Long press the saved image to delete it and click to see its dimensions ")
+                .setPositiveButton("OK", (dialog, which) -> {
+                    // Do something when the user clicks the positive button (OK)
+                    dialog.dismiss();
+                })
+                .setCancelable(false) // Prevent dismissing the dialog by tapping outside
                 .show();
     }
 }
