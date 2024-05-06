26622b4e6da28dd122bb8c1ac5a3384a0c5443bd
Sneha Ka Patel <133717181+kapa0029@users.noreply.github.com>
2024-05-06 15:42:48 -0400

    solved some errors

--- app/src/main/java/com/example/final_project_android/MainActivity.java
+++ app/src/main/java/com/example/final_project_android/MainActivity.java
@@ -44,45 +44,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-//         binding.bearImage.setOnClickListener(click-> {
-//             // Toast notification
-//             Toast.makeText(this, "Opening Bear Image Generator", Toast.LENGTH_SHORT).show();
-//             Intent intent = new Intent(MainActivity.this, Bear.class);
-//             startActivity(intent);
-//         });
-
-
-
-    
-//   @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//  }
-////    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//        if (id == R.id.bear_image) {
-//            startActivity(new Intent(this, Bear.class));
-//            return true;
-//        }
-//        else if (id==R.id.currency_converter) {
-//
-//
-//        }
-//        else if (id==R.id.trivia) {
-//
-//
-//        }
-//        else if (id==R.id.flight_tracker) {
-//            startActivity(new Intent(this, FlightTracker.class));
-//            return true;
-//        }
-//        else{
-//            return super.onOptionsItemSelected(item);
-//        }
-//        return false;
-//    }
 }
 
 
